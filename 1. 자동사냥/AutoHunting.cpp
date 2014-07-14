cd36155d94bc38a982491d73d1c4f965f734790f
Elenesgu <elenesgu@gmail.com>
2014-07-15 00:26:01 +0900

    Start fisrt assignment
    Create First Source File

---
+++ 1. 자동사냥/AutoHunting.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<list>
+
+using namespace std;
+
+#ifdef _LOCAL
+#include<fstream>
+ifstream in("input.txt");
+#else
+istream& in = cin;
+#endif
+
+int main() {
+
+	return 0;
+}
