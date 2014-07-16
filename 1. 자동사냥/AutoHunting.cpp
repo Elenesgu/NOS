0d60ca046080542b6a1a9ec8c95bd88e940c7f99
Elenesgu <elenesgu@gmail.com>
2014-07-16 12:00:30 +0900

    Make Weight-related functions
    Two inline functions: getWeight, getLength

--- 1. 자동사냥/AutoHunting.cpp
+++ 1. 자동사냥/AutoHunting.cpp
@@ -3,10 +3,9 @@
 #include<array>
 #include<vector>
 #include<functional>
+#include<cmath>
 
 using namespace std;
-
-
 
 #ifdef _LOCAL
 #include<sstream>
@@ -18,32 +17,43 @@ fstream in("input.txt");
 fstream out("output.txt");
 #endif
 
-
-
 typedef unsigned int num;
 
 struct Vector2 {
 	num x, y;
-	Vector2() :x(0), y(0) {};
 	Vector2(num ax, num ay) :x(ax), y(ay) {};
+	Vector2() :Vector2(0, 0) {};
 };
 
 struct Unit {
 	Vector2 coord;
 	num time;
 	num exp;
-	Unit(const Unit& obj) : coord(obj.coord.x, obj.coord.y), time(obj.time), exp(obj.exp) {};
 	Unit(num ax, num ay, num atime, num aexp) : coord(ax, ay), time(atime), exp(aexp) {};
 	Unit(num ax, num ay, num atime) : Unit(ax, ay, atime, 0) {};
 	Unit() : Unit(0, 0, 0, 0) {};
+	Unit(const Unit& obj) : Unit(obj.coord.x, obj.coord.y, obj.time, obj.exp) {};
 };
 
 typedef array<vector<Unit>, 8> Map;
 
-num EnemyToMap(int X, int Y);
+static num EnemyToMap(int X, int Y);
+
+void DoAutoHunting();
+
+inline static num getWeight(const Unit& origin, const Unit& target) {
+	return target.exp / (getLength(origin.coord, target.coord) + target.time);
+	// Reward / Time (to get the reward)
+}
+
+inline static num getLength(const Vector2& origin, const Vector2& target) {
+	return static_cast<num> (sqrt(pow(static_cast<double>(origin.x) - static_cast<double> (target.x), 2)
+		+ pow(static_cast<double>(origin.y) - static_cast<double>(target.y), 2)));
+}
 
 Map EnemyMap;
 array<int, 8> DirPriority;
+size_t HighPriority;
 Unit User;
 
 int main() {
@@ -63,16 +73,23 @@ int main() {
 		counter++;
 	}
 	counter = 0;
+	
+	num Max = 0;
 	while (counter < uCase) {
-		DirPriority[counter] = EnemyMap[counter].size();
+		DirPriority[counter] = EnemyMap[counter].size() + EnemyMap[(counter - 1) % 8].size() + EnemyMap[(counter + 1) % 8].size();
+		if (Max < DirPriority[counter]) {
+			HighPriority = counter;
+			Max = DirPriority[counter];
+		}
 		counter++;
-	}
+	}	
 	counter = 0;
+
 
 	return 0;
 }
 
-num EnemyToMap(int X, int Y) {
+static num EnemyToMap(int X, int Y) {
 	num whichMap = 0;
 	if (static_cast<int> (User.coord.x) <= X) {
 		if (static_cast<int> (User.coord.y) <= Y) {
